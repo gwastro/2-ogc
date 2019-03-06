6484eaec57f7bcc7fc94f78432ae1ab94b0361e5
Alexander Harvey Nitz <alex.nitz@gmail.com>
2019-03-06 15:46:20 +0000

    add times and scripts for O2 running

--- configuration/runO2.sh
+++ configuration/runO2.sh
@@ -1,5 +1,5 @@
-mkdir -p O1/$1
-cd O1/$1
+mkdir -p O2/$1
+cd O2/$1
 
 INI_PREFIX=../../
 
@@ -8,10 +8,10 @@ pycbc_make_coinc_search_workflow \
 --output-dir output \
 --config-files \
 ${INI_PREFIX}/analysis.ini \
-${INI_PREFIX}/data_O1.ini \
+${INI_PREFIX}/data_O2.ini \
 ${INI_PREFIX}/plotting.ini \
 ${INI_PREFIX}/injections_minimal.ini \
 ${INI_PREFIX}/executables.ini \
-${INI_PREFIX}/times/gps_times_O1_analysis_$1.ini \
+${INI_PREFIX}/times/gps_times_O2_analysis_$1.ini \
 --config-overrides \
 "results_page:output-path:../../../html"
